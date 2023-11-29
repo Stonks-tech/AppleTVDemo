273726b1f1a3f76f316863c6cd1f6c11574055c6
Jakub Neukirch <jakub.neukirch@gmail.com>
2023-11-29 08:47:42 +0100

    Initial Commit

---
+++ AppleTVDemo/AppleTVDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTVDemoApp.swift
+//  AppleTVDemo
+//
+//  Created by Jakub Neukirch on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleTVDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
